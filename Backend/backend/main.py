341a2974d753bfa741c6b2d363cf4d97ae6266cb
maicol <maicolenriquebruperez@gmail.com>
2025-07-03 11:25:14 -0500

    excel read

--- Backend/backend/main.py
+++ Backend/backend/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from sistemRAG import generate_response, search_similar_questions  # importa desde tu sistema actual
+from SistemRAG import generate_response, search_similar_questions  # importa desde tu sistema actual
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://udea-topaz.vercel.app"],  # o ["*"] si es temporal
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Carga tu DataFrame original desde disco (donde están las preguntas/respuestas)
-df = pd.read_json("preguntas_respuestas.json")  # o como lo hayas guardado
+df = pd.read_excel("PreguntasYRespuestas.xlsx")  # o como lo hayas guardado
 
 class QuestionRequest(BaseModel):
     question: str
