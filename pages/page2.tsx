d54c5fac4cde006ae3c69f3d1978662c8cb75996
AnalisisNumericoY <65009571+AnalisisNumericoY@users.noreply.github.com>
2025-07-23 13:39:58 -0500

    Upda page2.tsx

--- pages/page2.tsx
+++ pages/page2.tsx
@@ -37,7 +37,7 @@ export default function CustomizationPage() {
   setCurrentQuestion('');
 
   try {
-    const res = await fetch('https://f7c67788071d.ngrok-free.app/ask', {
+    const res = await fetch('https://1310779e4e51.ngrok-free.app/ask', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ question, empresa_id: empresaId, history: messages })
